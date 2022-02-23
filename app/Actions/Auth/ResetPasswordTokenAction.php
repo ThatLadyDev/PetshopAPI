9559406fcc71c9743cd04e43f45c9257e9f1da72
Lois Bassey <loisbassey@gmail.com>
2022-02-23 18:01:45 +0100

    reset password token functionality added

---
+++ app/Actions/Auth/ResetPasswordTokenAction.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Actions\Auth;
+
+use App\Http\Requests\Auth\ResetPasswordTokenRequest;
+use Exception;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Str;
+
+class ResetPasswordTokenAction
+{
+    public function execute(ResetPasswordTokenRequest $request) : void
+    {
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function ($user, $password) {
+                $user->forceFill([
+                    'password' => Hash::make($password)
+                ])->setRememberToken(Str::random(60));
+
+                $user->save();
+            }
+        );
+
+        if($status !== Password::PASSWORD_RESET){
+            throw new Exception(__($status));
+        }
+    }
+}
--- app/Http/Controllers/Authentication/ResetPasswordTokenController.php
+++ app/Http/Controllers/Authentication/ResetPasswordTokenController.php
@@ -3,12 +3,34 @@
 namespace App\Http\Controllers\Authentication;
 
 use App\Http\Controllers\Controller;
+use Exception;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use App\Http\Requests\Auth\ResetPasswordTokenRequest;
+use App\Actions\Auth\ResetPasswordTokenAction;
 
 class ResetPasswordTokenController extends Controller
 {
-    public function __invoke()
+    public function __invoke(ResetPasswordTokenRequest $request, ResetPasswordTokenAction $resetPasswordTokenAction)
     {
-        return "reset password";
+        try {
+            $resetPasswordTokenAction->execute($request);
+
+            return new JsonResponse([
+                'success' => 1,
+                'data'    => [
+                    'message' => 'Password has been successfully updated'
+                ],
+                'error'   => null,
+                'errors'  => [],
+                'extra'   => []
+            ], 200);
+        }
+        catch (Exception $e){
+            return new JsonResponse([
+                'success' => 0,
+                'error'   => $e->getMessage()
+            ], 500);
+        }
     }
 }
